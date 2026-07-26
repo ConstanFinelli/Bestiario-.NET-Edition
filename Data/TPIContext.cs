2e902b8dd753e34044f30d12b27d6a0c71497399
RicardoLCost <costamagnarl@gmail.com>
2026-07-26 18:56:23 -0300

    TPIContext adaptado

---
+++ Data/TPIContext.cs
@@ -0,0 +1,91 @@
+using Microsoft.EntityFrameworkCore;
+using Domain.Model;
+using Microsoft.Extensions.Configuration;
+
+namespace Data
+{
+    public class TPIContext : DbContext
+    {
+        public DbSet<Noticia> Noticias { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public TPIContext(DbContextOptions<TPIContext> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+            SeedInitialData();
+        }
+
+        internal TPIContext()
+        {
+            this.Database.EnsureCreated();
+            SeedInitialData();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                string connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+  
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Noticia>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd();
+                
+                entity.Property(e => e.Titulo)
+                    .IsRequired()
+                    .HasMaxLength(25);
+                
+                entity.Property(e => e.Contenido)
+                    .IsRequired()
+                    .HasMaxLength(150);
+                
+                entity.Property(e => e.FechaPublicacion)
+                    .IsRequired();
+                
+                /*entity.Navigation(e => e.Publicador)
+                    .HasField("_publicador");
+                    
+                entity.HasOne(e => e.Publicador)
+                    .WithMany()
+                    .HasForeignKey(e => e.PublicadorId); */
+            });
+
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd();
+                
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(20);
+                
+                entity.HasIndex(e => e.Nombre)
+                    .IsUnique();
+
+                // Datos iniciales
+                entity.HasData(
+                    new { Id = Guid.newGuid(), Nombre = "Artropodo" },
+                    new { Id = Guid.newGuid(), Nombre = "Anfibio" },
+                    new { Id = Guid.newGuid(), Nombre = "Aereo" },
+                    new { Id = Guid.newGuid(), Nombre = "Bipedo" },
+                    new { Id = Guid.newGuid(), Nombre = "Cuadrupedo" }
+                );
+            });
+}
