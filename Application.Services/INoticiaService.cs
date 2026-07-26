e82046da2f3bac27d21e166d5d299fa74fddc060
RicardoLCost <costamagnarl@gmail.com>
2026-07-26 19:40:49 -0300

    INoticiaService

---
+++ Application.Services/INoticiaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTOs;
+
+namespace Application.Services
+{
+    public interface INoticiaService
+    {
+        Task<NoticiaDTO> AddAsync(NoticiaDTO dto);
+        Task<bool> DeleteAsync(Guid id);
+        Task<NoticiaDTO?> GetAsync(Guid id);
+        Task<IEnumerable<NoticiaDTO>> GetAllAsync();
+        Task<bool> UpdateAsync(NoticiaDTO dto);
+    }
+}
